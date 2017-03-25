5fd6bd4357036dc27288435c7afccc173b05fff3
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-03-25 21:42:32 +0530

    added index.html in views.py

--- dobby/views.py
+++ dobby/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+import json
 
-# Create your views here.
+# from .models import Calender
+
+
+
+def index(request):
+	context = ""
+	return render(request, 'dobby/index.html', context)
