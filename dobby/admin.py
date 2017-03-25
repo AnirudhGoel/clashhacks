7edaa74c597002a4a026bfad32b7ea9c5938cdbe
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-03-25 22:13:29 +0530

    added more models

--- dobby/admin.py
+++ dobby/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-# from .models import Question
+from .models import Skill, Login, User
 
-# admin.site.register(Question)
+admin.site.register(Skill)
+admin.site.register(Login)
+admin.site.register(User)
+admin.site.register(Rating)
+admin.site.register(Transaction)
--- dobby/models.py
+++ dobby/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Skills(models.Model):
-	event_id = models.CharField(max_length=40, primary_key=True)
-	event_google_id = models.CharField(max_length=40, default="")
+class Skill(models.Model):
+	skillID = models.IntegerField(max_length=10, primary_key=True)
+	skillName = models.CharField(max_length=40, default="")
+
+class Login(models.Model):
+	userId = models.IntegerField(max_length=10, primary_key=True)
+	username = models.CharField(max_length=40, default="")
+	password = models.CharField(max_length=40, default="")
+
+class User(models.Model):
+	userId = models.IntegerField(max_length=10, primary_key=True)
+	Name = models.CharField(max_length=40)
+
+class Rating(models.Model):
+	userId = models.IntegerField(max_length=10)
+	skillID = models.IntegerField(max_length=10)
+	rating = models.IntegerField(max_length=1)
+
+class Transaction(models.Model):
+	teach = models.IntegerField(max_length=10)
+	learn = models.IntegerField(max_length=10)
+	byTime = models.BooleanField()
+	value = models.IntegerField(max_length=10)
