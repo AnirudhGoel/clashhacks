25fb229db0aea7d0f2b4b7f35078cd6eb06d28ab
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-10-30 14:04:04 +0530

    renamed clashhacks to skill-barter

--- skill-barter/urls.py
+++ skill-barter/urls.py
@@ -1,4 +1,4 @@
-"""clashhacks URL Configuration
+"""skill-barter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- skill-barter/wsgi.py
+++ skill-barter/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for clashhacks project.
+WSGI config for skill-barter project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clashhacks.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "skill-barter.settings")
 
 application = get_wsgi_application()
